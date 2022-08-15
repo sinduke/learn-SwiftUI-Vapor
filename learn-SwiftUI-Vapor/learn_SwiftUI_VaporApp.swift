efa27ec1ff7883606f4ac6157918a57f78d26cbb
kh <sinduke@outlook.com>
2022-08-15 16:50:37 +0800

    Initial Commit

---
+++ learn-SwiftUI-Vapor/learn_SwiftUI_VaporApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_SwiftUI_VaporApp.swift
+//  learn-SwiftUI-Vapor
+//
+//  Created by kh on 2022/8/15.
+//
+
+import SwiftUI
+
+@main
+struct learn_SwiftUI_VaporApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
